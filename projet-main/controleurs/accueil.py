ebeeee4b9b9ed971709dac4a3dbf6b4ae06d1289
mariepeachycheeks <125304963+mariepeachycheeks@users.noreply.github.com>
2024-11-08 14:14:45 +0100

    Update accueil.py

--- projet-main/controleurs/accueil.py
+++ projet-main/controleurs/accueil.py
@@ -6,3 +6,15 @@ from model.model_pg import count_instances
 
 res = count_instances(SESSION['CONNEXION'], 'legos.piece')
 REQUEST_VARS['nb_piece'] = res[0]
+    if res_partie:
+        REQUEST_VARS['nb_parties'] = res_partie[0]
+    else:
+        print("Error: res_partie is None or empty.")
+
+    res_joueuses = count_instances(SESSION['CONNEXION'], 'legos.JOUEUSE')
+    if res_joueuses:
+        REQUEST_VARS['nb_JOUEUSE'] = res_joueuses[0]
+    else:
+        print("Error: res_joueuses is None or empty.")
+except Exception as e:
+    print(f"An error occurred: {e}")
