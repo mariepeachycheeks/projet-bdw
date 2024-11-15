fb75d5a1fd09349339ebfaa4a5fcdabab60b41d6
mariepeachycheeks <125304963+mariepeachycheeks@users.noreply.github.com>
2024-11-15 14:29:31 +0100

    Update accueil.py

--- projet-main/controleurs/accueil.py
+++ projet-main/controleurs/accueil.py
@@ -2,7 +2,7 @@
 ce fichier est vide, il faudra y mettre du code (question TP5)
 """
 from server import SESSION, REQUEST_VARS
-from model.model_pg import count_instances
+from model.model_pg import count_instances, parties_pieces_defaussees_piochees, top_couleurs_nb_briques, score_min_max
 try:
     res_piece = count_instances(SESSION['CONNEXION'], 'legos.piece')
     if res_piece:
@@ -22,18 +22,29 @@ try:
     else:
         print("Error: res_joueuses is None or empty.")
 
-    res_top_couleurs = count_instances(SESSION['CONNEXION'], 'legos.piece')
+    res_top_couleurs = top_couleurs_nb_briques(
+        SESSION['CONNEXION'], 'legos.piece')
     if res_top_couleurs:
         REQUEST_VARS['top_couleurs'] = res_top_couleurs
     else:
         print("Error: res_joueuses is None or empty.")
 
-    res_max_min_score = count_instances(
+    res_max_min_score = score_min_max(
         SESSION['CONNEXION'], 'legos.JOUEUSE', 'legos.LIER', 'legos.PARTIE')
     if res_max_min_score:
         REQUEST_VARS['max_min_score'] = res_top_couleurs
     else:
         print("Error: res_max_min_score is None or empty.")
+
+    res_pieces_defaussees_piochees = parties_pieces_defaussees_piochees(
+        SESSION['CONNEXION'], 'legos.PARTIE')
+    if res_pieces_defaussees_piochees:
+        REQUEST_VARS['piochees_min'] = res_pieces_defaussees_piochees[0]
+        REQUEST_VARS['piochees_max'] = res_pieces_defaussees_piochees[1]
+        REQUEST_VARS['defaussees_min'] = res_pieces_defaussees_piochees[2]
+        REQUEST_VARS['defaussees_max'] = res_pieces_defaussees_piochees[3]
+    else:
+        print("Error: res_pieces_defaussees_piochees is None or empty.")
 
 
 except Exception as e:
