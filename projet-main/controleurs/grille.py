7f24111398b8be42f7370f3757edc498a7555509
Nonamehaha45 <145796276+Nonamehaha45@users.noreply.github.com>
2024-11-20 12:58:58 +0100

    Update grille.py

--- projet-main/controleurs/grille.py
+++ projet-main/controleurs/grille.py
@@ -27,8 +27,15 @@ REQUEST_VARS['episodes2'] = get_episodes_for_num(SESSION['CONNEXION'], 2)
 
 """Fonctionnalité 2""""
 
-REQUEST_VARS['piece'] = get_random_bricks(SESSION['CONNEXION'])
 
+REQUEST_VARS['piece'] = get_random_brick(SESSION['CONNEXION'])
+
+ if piece is None:
+        logger.warning("Il y a plus de bricks dans BD")
+        return []
+
+    
+    
 initialize_pioche(SESSION['CONNEXION'], 4)
 
 selected_id = REQUEST_VARS['selected_id']
@@ -37,6 +44,34 @@ if selected_id:
     REQUEST_VARS['piece'] = replace_selected_brick(SESSION['CONNEXION'], selected_id)
 else:
     logger.error("Selected brick ID is missing.")
+
+
+
+
+
+'''doma screen '''
+
+pioche=[]
+
+REQUEST_VARS['piece'] = b
+
+
+if b is None:
+        logger.warning("Il y a plus de bricks dans BD")
+        return []
+
+    
+if 'pioche' not in SESSION:
+    SESSION['pioche']=get_random_brick(SESSION['CONNEXION'], 4)
+
+    if form_submitted:
+
+        if b in SESSION['pioche']:
+
+         SESSION['pioche'].remove(b)
+
+     SESSION['pioche'].extend(get_random_brick(SESSION['CONNEXION'], 1))
+
 
 
 """Fonctionnalité 4""""
